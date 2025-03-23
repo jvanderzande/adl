e0ac4adf7d8aae3d91c35bf2ae0e02d80b5ad00b
Sean McCollum <anincrediblyshortname@gmail.com>
2025-03-22 18:22:46 -0700

    throw exception when completely broken thing happens

--- adl/__init__.py
+++ adl/__init__.py
@@ -1,13 +1,9 @@
-import logging
-import sys
-
+from .exceptions import DataDirectoryAccessError
 from . import db
 
-logger = logging.getLogger(__name__)
 data = db.DBData()
 
 if not data.check_or_create_dir():
-  logger.error("Error accessing data directory !")
-  sys.exit(1)
+  raise DataDirectoryAccessError()
 
 data.load()
